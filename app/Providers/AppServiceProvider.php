24b62e4485a8b1e19e14dd6c2058f2a58d4dc2b6
Wuttchai <champsamui8@gmail.com>
2018-09-07 23:52:24 +0700

    t

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,6 +11,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+      URL::forceScheme('https');
+      $this->app['request']->server->set('HTTPS', true);
     }
 
     /**
